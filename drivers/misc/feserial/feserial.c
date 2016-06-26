f3c795d22fbaf258de1d249c9cc9e16a7dfd9137
Martin Kelly <martin@surround.io>
2016-06-25 19:03:00 -0700

    write operation works

--- drivers/misc/feserial/feserial.c
+++ drivers/misc/feserial/feserial.c
@@ -1,12 +1,19 @@
+#include <linux/fs.h>
 #include <linux/init.h>
 #include <linux/io.h>
+#include <linux/miscdevice.h>
 #include <linux/module.h>
 #include <linux/platform_device.h>
 #include <linux/pm_runtime.h>
 #include <linux/of.h>
 #include <linux/serial_reg.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
+
+#define FESERIAL_NAME "feserial"
 
 struct feserial_dev {
+	struct miscdevice miscdev;
 	void __iomem *regs;
 };
 
@@ -18,17 +25,46 @@ void reg_write(struct feserial_dev *dev, unsigned int val, int off) {
 	writel(val, dev->regs + 4*off);
 }
 
-void test_write_char(struct feserial_dev *dev, char c) {
+void uart_write(struct feserial_dev *dev, char c) {
 	while (!(reg_read(dev, UART_LSR) & UART_LSR_THRE))
 		cpu_relax();
 
 	reg_write(dev, c, UART_TX);
 }
 
+
+static ssize_t feserial_read(struct file *file, char __user *buf, size_t count,
+	loff_t *offset) {
+	return -EINVAL;
+}
+
+static ssize_t feserial_write(struct file *file, const char __user *buf,
+	size_t count, loff_t *offset) {
+	char c;
+	struct feserial_dev *dev;
+	int i;
+
+	dev = container_of(file->private_data, struct feserial_dev, miscdev);
+	for (i = 0; i < count; i++) {
+		get_user(c, &buf[i]);
+		uart_write(dev, c);
+	}
+
+	return count;
+}
+
+static const struct file_operations fops = {
+	.owner    = THIS_MODULE,
+	.read   = feserial_read,
+	.write    = feserial_write,
+};
+
+
 static int feserial_probe(struct platform_device *pdev)
 {
 	unsigned int baud_divisor;
 	struct feserial_dev *dev;
+	const char *name;
 	struct resource *res;
 	unsigned int uartclk;
 
@@ -51,6 +87,8 @@ static int feserial_probe(struct platform_device *pdev)
 		return PTR_ERR(dev->regs);
 	}
 
+	platform_set_drvdata(pdev, dev);
+
 	pm_runtime_enable(&pdev->dev);
 	pm_runtime_get_sync(&pdev->dev);
 
@@ -68,18 +106,25 @@ static int feserial_probe(struct platform_device *pdev)
 	reg_write(dev, UART_FCR_CLEAR_RCVR | UART_FCR_CLEAR_XMIT, UART_FCR);
 	reg_write(dev, 0x00, UART_OMAP_MDR1);
 
-	test_write_char(dev, 'a');
-	test_write_char(dev, 'b');
-	test_write_char(dev, 'c');
-	test_write_char(dev, 'd');
+	/* Setup misc device. */
+	dev->miscdev.minor = MISC_DYNAMIC_MINOR;
+	name = kasprintf(GFP_KERNEL, FESERIAL_NAME "-%x", res->start);
+	dev->miscdev.name = name;
+	dev->miscdev.fops = &fops;
+	misc_register(&dev->miscdev);
 
 	return 0;
 }
 
 static int feserial_remove(struct platform_device *pdev)
 {
+	struct feserial_dev *dev;
+
 	pr_info("Called feserial_remove\n");
 
+	dev = platform_get_drvdata(pdev);
+	misc_deregister(&dev->miscdev);
+	kfree(dev->miscdev.name);
 	pm_runtime_disable(&pdev->dev);
 
 	return 0;
@@ -95,7 +140,7 @@ MODULE_DEVICE_TABLE(of, feserial_of_match);
 
 static struct platform_driver feserial_driver = {
 	.driver = {
-		.name = "feserial",
+		.name = FESERIAL_NAME,
 		.owner = THIS_MODULE,
 		.of_match_table = feserial_of_match
 	},
